0cb23c610676b0e360656cefc2883032c1155219
enes <enes@DESKTOP-V33KVOR>
2021-08-20 23:23:34 +0300

    Add project files.

---
+++ Skill_CodeFirstEntity/Controllers/AdminController.cs
@@ -0,0 +1,15 @@
+﻿using Skill_CodeFirstEntity.Models.siniflar;
+using System.Web.Mvc;
+namespace Skill_CodeFirstEntity.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            CONTEXT c = new CONTEXT();
+            var degerler = c.YETENEKLERs.ToString();
+            return View (degerler);
+        }
+    }
+}
